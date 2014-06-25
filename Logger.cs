9b73fb19085fcd8aff52452309377457230afc00
Jasper van Bourgognie <van.bourgognie@gmail.com>
2014-06-25 12:04:54 +0200

    Initial commit (logger)

---
+++ Logger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public enum LogLevel{
+        ERROR = 0,
+        WARN = 10,
+        INFO = 20,
+        DEBUG = 30,
+    }
+    public class LogMessage
+    {
+        public LogLevel level { get; private set; }
+        public string message { get; private set; }
+        public LogMessage(LogLevel l, string msg)
+        {
+            this.message = msg;
+            this.level = l;
+        }
+    }
+    public class Logger
+    {
+        public delegate void logUpdateCallback();
+        static List<Queue<LogMessage>> logQueues = new List<Queue<LogMessage>>();
+        static List<logUpdateCallback> logUpdateCallbacks = new List<logUpdateCallback>();
+        public static void AddQueue(Queue<LogMessage> q, logUpdateCallback cb = null)
+        {
+            logQueues.Add(q);
+            if(cb != null)
+                logUpdateCallbacks.Add(cb);
+        }
+        private static void Log(LogLevel l, string msg)
+        {
+            foreach(var q in logQueues)
+                q.Enqueue(new LogMessage(l, msg));
+            foreach (var cb in logUpdateCallbacks)
+                cb();
+        }
+        public static void Info(string text) {  Log(LogLevel.INFO, text); }
+        public static void Debug(string text) { Log(LogLevel.DEBUG, text); }
+        public static void Warn(string text) { Log(LogLevel.WARN, text); }
+        public static void Error(string text) { Log(LogLevel.ERROR, text); }
+    }
+}
