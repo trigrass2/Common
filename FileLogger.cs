271308ea537feda67ca37d2c621ef5804ea983ea
Jasper van Bourgognie <van.bourgognie@gmail.com>
2014-07-04 09:17:14 +0200

    Moved FileLogger and LinqExtensions to Common

---
+++ FileLogger.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace Common
+{
+    public class FileLogger
+    {
+        string path;
+        FileStream stream;
+        StreamWriter writer;
+        Thread dumpThread;
+        ConcurrentQueue<LogMessage> logQueue;
+        bool running;
+        LogLevel logLevel;
+
+        public FileLogger(string filename, LogLevel level)
+        {
+            this.logLevel = level;
+            path = Path.Combine(Common.Utils.StoragePath, filename);
+            stream = new FileStream(this.path, FileMode.Append);
+            writer = new StreamWriter(stream);
+            logQueue = new ConcurrentQueue<LogMessage>();
+            Logger.AddQueue(logQueue);
+
+
+            dumpThread = new Thread(dumpThreadStart);
+            running = true;
+            dumpThread.Start();
+	        Logger.Info("Started file logger in " + path);
+        }
+
+        public void Stop()
+        {
+            this.running = false;
+            dumpThread.Join();
+        }
+
+        private void dumpThreadStart()
+        {
+            while (running)
+            {
+                while (logQueue.Count > 0)
+                {
+                    LogMessage entry;
+                    if (logQueue.TryDequeue(out entry))
+                    {
+                        if (entry.level > logLevel) continue;
+                        string message = entry.timestamp.ToString().PadRight(22) + entry.level.ToString().PadRight(15) + entry.message;
+                        writer.WriteLine(message);
+                    }
+                }
+                writer.Flush();
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
---
+++ LinqExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public static class Extensions
+    {
+        public static float StdDev(this IEnumerable<float> values)
+        {
+            float ret = 0;
+            int count = values.Count();
+            if (count > 1)
+            {
+                //Compute the Average
+                double avg = values.Average();
+
+                //Perform the Sum of (value-avg)^2
+                double sum = values.Sum(d => (d - avg) * (d - avg));
+
+                //Put it all together
+                ret = (float)Math.Sqrt(sum / count);
+            }
+            return ret;
+        }
+
+        public static float Median(this IEnumerable<float> source)
+        {
+            int decimals = source.Count();
+
+            int midpoint = (decimals - 1) / 2;
+            IEnumerable<float> sorted = source.OrderBy(n => n);
+            
+            float median = sorted.ElementAt(midpoint);
+            if (decimals % 2 == 0)
+                median = (median + sorted.ElementAt(midpoint + 1)) / 2;
+
+            return median;
+        }
+    }
+}
